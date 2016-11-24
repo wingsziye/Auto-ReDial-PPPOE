f952fc914becb5eb36b97eb9b2a2ea76d47c8480
wingsziye <a237469733@gmail.com>
2016-11-24 09:29:29 +0800

    FirstUpload
    FirstUpload

---
+++ 宽带/MetroAutoDialNet4/DialManager.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotRas;
+using DotRas.Design;
+using System.Threading;
+using System.Windows.Controls;
+using System.Collections.ObjectModel;
+using System.Runtime.Remoting.Messaging;
+using System.Threading.Tasks;
+
+namespace MetroAutoDialNet4
+{
+    public class DialManager
+    {
+        public event Action<string> DialgLostConnection;
+        public event Action<string> StopListenen;
+        public void Diag(string entryName)
+        {
+            //cmb_ActiveConnectionList = cmb;
+            try
+            {
+                RasDialer dialer = new RasDialer();
+                dialer.EntryName = entryName;
+                //dialer.PhoneNumber = " ";
+                dialer.AllowUseStoredCredentials = true;
+                dialer.PhoneBookPath = RasPhoneBook.GetPhoneBookPath(RasPhoneBookType.AllUsers);
+                dialer.Timeout = 1000;
+                dialer.Dial();
+
+                Thread.Sleep(100);
+                //this.LoadConnections();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public void Disconnect(string entryName)
+        {
+            ReadOnlyCollection<RasConnection> conList = RasConnection.GetActiveConnections();
+            foreach (RasConnection con in conList)
+            {
+                Console.WriteLine(con.EntryName);
+                if (con.EntryName == entryName)
+                {
+                    con.HangUp();
+                }
+            }
+        }
+
+        public string[] ListActiveConnName()
+        {
+            ReadOnlyCollection<RasConnection> conList = RasConnection.GetActiveConnections();
+            List<string> s = new List<string>();
+            DateTime date = DateTime.Now;
+            foreach (RasConnection con in conList)
+            {
+                //Console.WriteLine(date + ":" + con.EntryName);
+                s.Add(con.EntryName);
+            }
+            return s.ToArray();
+        }
+
+        #region 定时器方法
+        /*
+        DialThreadParam timeDtp = new DialThreadParam();
+        public void TimeStartListen( int intervalMillSecond, string listenEntryName)
+        {
+            timeDtp.IntervalMilliSecTime = intervalMillSecond;
+            timeDtp.ListenEntryName = listenEntryName;
+            Timer t = new Timer((object a)=> {  });
+        }
+        */
+        #endregion
+
+        #region 线程方法
+        bool cnaListen;
+
+        public bool CnaListen
+        {
+            get
+            {
+                return cnaListen;
+            }
+
+            set
+            {
+                cnaListen = value;
+            }
+        }
+
+        public string CanListenCheck(string listenEntryName)
+        {
+            bool canListen = false;
+            var conList = RasConnection.GetActiveConnections();
+            foreach (var item in conList)
+            {
+                if (item.EntryName == listenEntryName)
+                {
+                    canListen = true;
+                }
+            }
+            if (canListen)
+            {
+                Console.WriteLine("+1");
+                CnaListen = true;
+                return "已找到: \"" + listenEntryName + "\" 任务开始...";
+            }
+            else
+            {
+                CnaListen = false;
+                Console.WriteLine("-1");
+                return "未找到活动的拨号连接: " + listenEntryName;
+            }
+        }
+
+        ThreadWorkClass workclass;
+        public void StartListen(int intervalMilliSecTime, string listenName)
+        {            
+            workclass = new ThreadWorkClass();
+            workclass.intervalMilliSecTime = intervalMilliSecTime;
+            workclass.listenEntryName = listenName;
+            workclass.DisConnectCalback = new ThreadWorkClass.ThreadCallBackDelegate(ThreadCallback);
+            workclass.StopConnectCalback = new ThreadWorkClass.ThreadCallBackDelegate(StopThreadCallback);
+            Thread th = new Thread(ThreadWork);
+            th.Start(workclass);//开始线程，代入参数
+        }
+
+        private void ThreadWork(object obj)
+        {
+            var workcalss = (ThreadWorkClass)obj;
+            
+            while (true)
+            {
+                if (workcalss.isStopThread)
+                {
+                    break;//break退出while循环
+                }
+                Thread.Sleep(workcalss.intervalMilliSecTime);
+                bool state = false;//fasle = Disconn; true = Alive;
+                var conList = RasConnection.GetActiveConnections();
+                //Console.WriteLine(DateTime.Now);
+                for (int i = 0; i < conList.Count; i++)
+                {
+                    if (conList[i].EntryName == workcalss.listenEntryName)
+                    {
+                        state = true;
+                        //break;
+                    }
+                }
+                if (state == false)//如果没检测到连接，则表示已断开
+                {
+                    //break;
+                    workcalss.DisConnectCalback(DateTime.Now+" : 检测到已断开，执行重连中...");
+                }
+            }
+            workcalss.isStopThread = false;
+            workcalss.StopConnectCalback("任务结束，已停止 \"" + workcalss.listenEntryName + "\" 的检测");
+        }
+
+        class ThreadWorkClass
+        {
+            public delegate void ThreadCallBackDelegate(string message);
+            public ThreadCallBackDelegate DisConnectCalback;
+            public ThreadCallBackDelegate StopConnectCalback;
+            public int intervalMilliSecTime;
+            public string listenEntryName;
+            public bool isStopThread = false;
+        }
+
+        public void StopListen()
+        {
+            if (workclass!=null)
+            {
+                workclass.isStopThread = true;
+            }
+        }
+
+        private void ThreadCallback(string message)
+        {
+            Console.WriteLine(message);
+            if (this.DialgLostConnection!=null)
+            {
+                this.DialgLostConnection(message);
+            }
+        }
+
+        private void StopThreadCallback(string message)
+        {
+            Console.WriteLine(message);            
+            if (this.StopListenen!=null)
+            {
+                this.StopListenen(message);
+            }
+            
+        }
+        
+        #endregion
+
+        #region TaskTest
+        /*
+        DialThreadParam dtp = new DialThreadParam();
+
+
+        public async Task<string> WaitAsynchronouslyAsync(int interval,string entryName)
+        {
+            //await Task.Delay(10000);
+            dtp.ListenEntryName = entryName;
+            dtp.IntervalMilliSecTime = interval;
+            string result = await Task.Run(() => dtp.WaitDialGoOffline());
+            return result;
+        }
+        */
+        #endregion
+
+    }
+
+    public class DialThreadParam
+    {
+        int intervalMilliSecTime;
+        public int IntervalMilliSecTime
+        {
+            get
+            {
+                return intervalMilliSecTime;
+            }
+
+            set
+            {
+                intervalMilliSecTime = value;
+            }
+        }
+        private string listenEntryName;
+        public string ListenEntryName
+        {
+            get
+            {
+                return listenEntryName;
+            }
+
+            set
+            {
+                listenEntryName = value;
+            }
+        }
+        public string WaitDialGoOffline()
+        {
+            var tw = this;
+            string retMessage = string.Empty;
+            while (true)
+            {
+                Thread.Sleep(tw.IntervalMilliSecTime);
+                bool state = false;//fasle = Disconn; true = Alive;
+                var conList = RasConnection.GetActiveConnections();
+                Console.WriteLine(DateTime.Now);
+                for (int i = 0; i < conList.Count; i++)
+                {
+                    if (conList[i].EntryName == tw.ListenEntryName)
+                    {
+                        state = true;
+                        break;
+                    }
+                }
+                if (state == false)//如果没检测到连接，则表示已断开
+                {
+                    break;
+                }
+            }
+            retMessage = tw.ListenEntryName + "断开咯";
+            return retMessage;
+        }
+
+
+
+        public string tasktest()
+        {
+            Thread.Sleep(10000);
+            return "a";
+        }
+    }
+}
---
+++ 宽带/MetroAutoDialNet4/MainWindow.xaml.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using System.ComponentModel;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace MetroAutoDialNet4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        ObservableCollection<UserMessage> UserMessageList;
+        DialManager dialManager;
+        ComboBoxItem cmbi_default;
+        string selDialName;
+        int intervalSecond;
+
+        public string SelDialName
+        {
+            get
+            {
+                return selDialName;
+            }
+
+            set
+            {
+                selDialName = value;
+                //Console.WriteLine(value);
+            }
+        }
+
+        public int IntervalSecond
+        {
+            get
+            {
+                return intervalSecond;
+            }
+
+            set
+            {
+                intervalSecond = value;
+            }
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            UserMessageList = new ObservableCollection<UserMessage>();
+            lv_msgview.ItemsSource = UserMessageList;
+            dialManager = new DialManager();
+            dialManager.DialgLostConnection += DialManager_DialgLostConnection;
+            dialManager.StopListenen += DialManager_StopListenen;
+            cmbi_default = cmb_linklist.Items[0] as ComboBoxItem;
+        }
+
+        private void DialManager_StopListenen(string obj)
+        {
+            this.Dispatcher.BeginInvoke(new Action(() => {
+                InputMessage(obj);                
+            }));
+        }
+
+        private void DialManager_DialgLostConnection(string obj)
+        {
+            this.Dispatcher.BeginInvoke(new Action(() => {
+                InputMessage(obj);
+                invokeDial();
+                InputMessage("重连\""+SelDialName+"\"完成");
+            }));
+        }
+
+        private void InputMessage(string message)
+        {
+            if (UserMessageList != null)
+            {
+                if (UserMessageList.Count > 0)
+                {
+                    if (UserMessageList[UserMessageList.Count - 1].Numbers != UserMessageList.Count)
+                    {
+                        flashNumbers(UserMessageList);//当列表序号不对时，刷新列表顺序
+                    }
+                }
+                UserMessageList.Add(new UserMessage() { Numbers = UserMessageList.Count + 1, Message = message });
+                lv_msgview.ScrollIntoView(lv_msgview.Items[lv_msgview.Items.Count - 1]);
+            }
+        }
+
+        private void deleteMessage(int startindex)
+        {
+            if (UserMessageList != null)
+            {
+                UserMessageList.RemoveAt(startindex);
+            }
+        }
+
+        private async void flashNumbers(ObservableCollection<UserMessage> uc)
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < uc.Count; i++)
+                {
+                    uc[i].Numbers = i + 1;
+                    Console.WriteLine("线程中");
+                }
+            });
+        }
+
+        private void btn_stopListen_Click(object sender, RoutedEventArgs e)
+        {
+            InputMessage("正在停止..请稍后...");
+            dialManager.StopListen();
+            #region 启用按钮
+            btn_dial.IsEnabled = true;
+            btn_flashMsg.IsEnabled = true;
+            #endregion
+        }
+
+        private void btn_startListen_Click(object sender, RoutedEventArgs e)
+        {
+            #region Task方法,只能执行一次
+            /*
+            InputMessage(dialManager.CanListenCheck(SelDialName));
+            if (dialManager.CnaListen)
+            {
+                var result = await dialManager.WaitAsynchronouslyAsync(IntervalSecond, SelDialName);
+                InputMessage(result);
+            }
+            */
+            #endregion
+
+            #region 禁用按钮
+            btn_dial.IsEnabled = false;
+            btn_flashMsg.IsEnabled = false;
+            #endregion
+
+            #region 线程while方法
+            InputMessage(dialManager.CanListenCheck(SelDialName));
+            if (dialManager.CnaListen)
+            {
+                dialManager.StartListen(IntervalSecond, SelDialName);
+            }
+            #endregion
+
+        }
+
+        private void invokeDial()
+        {
+            InputMessage("正在尝试对 \"" + SelDialName + "\" 进行拨号...");
+            
+            dialManager.Diag(SelDialName);
+        }
+
+        private void btn_dial_Click(object sender, RoutedEventArgs e)
+        {            
+            if (SelDialName != "" && SelDialName != null)
+            {
+                dialManager.Diag(SelDialName);
+            }
+        }
+
+        private void btn_flashMsg_Click(object sender, RoutedEventArgs e)
+        {
+            UserMessageList.Clear();
+            clearcmb_linkList();
+
+            var items = dialManager.ListActiveConnName();
+            foreach (var item in items)
+            {
+                cmb_linklist.Items.Add(item);
+            }
+        }
+
+        private void clearcmb_linkList()
+        {
+            cmb_linklist.Items.Clear();
+            cmb_linklist.Items.Add(cmbi_default);
+            cmb_linklist.SelectedIndex = 0;
+        }
+
+        private void cmb_linklist_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox a = sender as ComboBox;
+            if (a.SelectedIndex == 0)
+            {                
+                SelDialName = tbx_manulinput.Text;
+                InputMessage("已选择手动输入...");
+            }
+            else if (a.SelectedIndex > 0)
+            {
+                SelDialName = a.Items[a.SelectedIndex].ToString();
+                InputMessage("已选择 \""+ "\""+ SelDialName);
+            }
+        }
+
+        private void tbx_manulinput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (cmb_linklist.SelectedIndex == 0)
+            {
+                SelDialName = ((TextBox)sender).Text;
+            }
+        }
+
+        private void tbx_interval_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var t = (TextBox)sender;
+            string text = t.Text;
+            int a = 5000;
+            if (text!=string.Empty)
+            {                
+                try
+                {
+                    a = int.Parse(text);
+                }
+                catch (Exception)
+                {
+                    //t.Text = t.Text.Remove(t.Text.Length - 1, 1);
+                    t.Text = a.ToString();
+                }
+            }
+            IntervalSecond = a;
+        }
+    }
+
+    public class UserMessage : INotifyPropertyChanged
+    {
+        int numbers;
+
+        public int Numbers
+        {
+            get
+            {
+                return numbers;
+            }
+
+            set
+            {
+                numbers = value;
+                OnPropertyChanged(nameof(Numbers));
+            }
+        }
+
+        public string Message
+        {
+            get
+            {
+                return message;
+            }
+
+            set
+            {
+                message = value;
+                OnPropertyChanged(nameof(Message));
+            }
+        }
+
+        string message;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string Name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(Name));
+            }
+        }
+    }
+}
